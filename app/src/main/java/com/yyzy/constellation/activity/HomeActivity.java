f0d80bd3cd1df17b4a8497f1a6b0798d421b8fce
Phang86 <1453061838@qq.com>
2022-06-11 12:14:30 +0800

    引导页更新

--- app/src/main/java/com/yyzy/constellation/activity/HomeActivity.java
+++ app/src/main/java/com/yyzy/constellation/activity/HomeActivity.java
@@ -8,13 +8,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
 import android.widget.TextView;
 
 import com.yyzy.constellation.R;
 
 public class HomeActivity extends BaseActivity {
     private TextView tv;
-    private int time = 4;
+    private int time = 5;
     private SharedPreferences firstSpf;
 
     @Override
@@ -27,6 +28,16 @@ public class HomeActivity extends BaseActivity {
         tv = findViewById(R.id.home_tv);
         handler.sendEmptyMessageDelayed(1,1000);
         firstSpf = getSharedPreferences("first_spf",MODE_PRIVATE);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(HomeActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                handler.removeCallbacksAndMessages(null);
+            }
+        });
     }
 
     @Override
@@ -38,29 +49,30 @@ public class HomeActivity extends BaseActivity {
         @Override
         public void handleMessage(@NonNull Message msg) {
             super.handleMessage(msg);
-            if (msg.what == 1) {
-                time--;
-                if (time == 0) {
-                    //判断是否第一次进入
-                    boolean isFirst = firstSpf.getBoolean("isFirst", true);
-                    Intent intent = new Intent();
-                    if (isFirst){
-                        intent.setClass(HomeActivity.this,WelcomeActivity.class);
-                        //为了下一次不进行跳转引导界面，把状态设置为false
-                        SharedPreferences.Editor edit = firstSpf.edit();
-                        edit.putBoolean("isFirst",false);
-                        edit.commit();
+                if (msg.what == 1) {
+                    time--;
+                    if (time == 0) {
+                        //判断是否第一次进入
+                        boolean isFirst = firstSpf.getBoolean("isFirst", true);
+                        Intent intent = new Intent();
+                        if (isFirst){
+                            intent.setClass(HomeActivity.this,WelcomeActivity.class);
+                            //为了下一次不进行跳转引导界面，把状态设置为false
+                            SharedPreferences.Editor edit = firstSpf.edit();
+                            edit.putBoolean("isFirst",false);
+                            edit.commit();
+                        }else {
+                            //不是第一次进入  直接跳过引导页面进入
+                            intent.setClass(HomeActivity.this,LoginActivity.class);
+                        }
+                        startActivity(intent);
+                        finish();
                     }else {
-                        //不是第一次进入  直接跳过引导页面进入
-                        intent.setClass(HomeActivity.this,LoginActivity.class);
+                        tv.setText("倒计时"+String.valueOf(time)+"秒"+"\t点击跳过");
+                        handler.sendEmptyMessageDelayed(1,1000);
                     }
-                    startActivity(intent);
-                    finish();
-                }else {
-                    tv.setText("倒计时\t"+String.valueOf(time)+"秒");
-                    handler.sendEmptyMessageDelayed(1,1000);
                 }
-            }
         }
     };
+
 }
