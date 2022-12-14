c0e9508bdfe71f58d806ad914ecfcad40033def6
Phang86 <1453061838@qq.com>
2022-12-14 22:44:21 +0800

    update

--- app/src/main/java/com/yyzy/constellation/utils/Constellation.java
+++ app/src/main/java/com/yyzy/constellation/utils/Constellation.java
@@ -18,6 +18,9 @@ import com.yyzy.constellation.weather.db.DBManager;
 
 import org.xutils.x;
 
+import skin.support.SkinCompatManager;
+import skin.support.app.SkinAppCompatViewInflater;
+
 
 public class Constellation extends Application {
     private static RequestQueue httpQueue;
@@ -39,11 +42,11 @@ public class Constellation extends Application {
         //初始化图片加载框架ImageLoader
         initImageLoader(getApplicationContext());
 
-//        SkinCompatManager.withoutActivity(this)
-//                .addInflater(new SkinAppCompatViewInflater())           // 基础控件换肤初始化
-//                .setSkinStatusBarColorEnable(false)                     // 关闭状态栏换肤，默认打开[可选]
-//                .setSkinWindowBackgroundEnable(false)                   // 关闭windowBackground换肤，默认打开[可选]
-//                .loadSkin();
+        SkinCompatManager.withoutActivity(this)
+                .addInflater(new SkinAppCompatViewInflater())           // 基础控件换肤初始化
+                .setSkinStatusBarColorEnable(false)                     // 关闭状态栏换肤，默认打开[可选]
+                .setSkinWindowBackgroundEnable(false)                   // 关闭windowBackground换肤，默认打开[可选]
+                .loadSkin();
 //        SharedPreferences sp = getSharedPreferences("sp_ttit",MODE_PRIVATE);
 //        String skin = sp.getString("skin", "");
 //        if (skin.equals("night")){
